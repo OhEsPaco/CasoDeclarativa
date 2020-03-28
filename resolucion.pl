be34ee938a5aa79db10b410a7cd7954751802bb5
OhEsPaco <fcomanuel.garcia4@alu.uclm.es>
2020-03-28 23:43:53 +0100

    Puesta bonita mi parte

--- resolucion.pl
+++ resolucion.pl
@@ -4,12 +4,6 @@
 :-op(700,xfy,->).
 :-op(700,xfy,<->).
 
-
-
-/*Toma una lista de premisas y las une mediante & y añade la conclusion negada*/
-addPremisesAndNegateConclussion([],Conclussion,~Conclussion).
-addPremisesAndNegateConclussion([H|T],Conclussion,H&Conc):-addPremisesAndNegateConclussion(T,Conclussion,Conc).
-
 /*Dada una lista con las premisas y la conclusion devuelve una lista de clausulas*/
 translate(Premises,Conclussion,Clauses) :-
 addPremisesAndNegateConclussion(Premises,Conclussion,X0), /* Stage 0*/
@@ -19,6 +13,10 @@ skolem(X2,X3,[]), /* Stage 3 */
 univout(X3,X4), /* Stage 4 */
 conjn(X4,X5), /* Stage 5 */
 clausify(X5,Clauses,[]). /* Stage 6 */
+
+/*Toma una lista de premisas y las une mediante & y añade la conclusion negada*/
+addPremisesAndNegateConclussion([],Conclussion,~Conclussion).
+addPremisesAndNegateConclussion([H|T],Conclussion,H&Conc):-addPremisesAndNegateConclussion(T,Conclussion,Conc).
 
 implout((P<->Q),((P1 & Q1)#(~P1 & ~Q1))):-!,implout(P,P1),implout(Q,Q1).
 implout((P->Q),(~P1 # Q1)):-!,implout(P,P1),implout(Q,Q1).
@@ -93,58 +91,85 @@ test1():-esArgumentoCorrecto([~ p -> q & r, s & q-> ~ r,t -> ~ u # s],t -> ~u # 
 test2():-esArgumentoCorrecto([p -> r , q <-> r,q & s, s->t,p],t).
 
 
-/*translate(Premises,Conclussion,Clauses) :-*/
+%Dada una lista de premisas y una conclusion deduce si un argumento es correcto
 esArgumentoCorrecto(Premisas,Conclussion):-translate(Premisas,Conclussion,Clauses),sld(Clauses).
 
 
-%Funciona
-%sld(_,N):-N>=10000,nl,write("No encontrado en tiempo razonable").
-%sld(Clausulas,N):-N<10000,NN is N + 1,Clausulas=[H|T],resolver(H,T,Resolvente,Compatible),((Resolvente\==cl([],[]))->
-%((Compatible=no)->(append(T,[H],NClausulas),sld(NClausulas,NN));(append(T,[Resolvente,H],NClausulas),sld(NClausulas,NN)));nl,write(Resolvente),true).
+%Dada una lista de clausulas ejecuta resolucion sobre ellas
+sld(Clauses):-
+	sld(Clauses,Clauses). %Hay que tener cuidado al generar un resolvente de que no esté entre las clasulas originales
 
-%Parece que funciona mejor
-%sld(_,N):-N>=10000,nl,write("No encontrado en tiempo razonable").
-%sld(Clausulas,N):-N<10000,NN is N + 1,Clausulas=[H|T],resolver(H,T,Resolvente,Compatible),((Resolvente\==cl([],[]))->
-%((Compatible=no)->(sld(T,NN));(append([Resolvente|T],[H],NClausulas),sld(NClausulas,NN)));nl,write(Resolvente),true).
+sld([H|T],Clausulas):-
+	resolver(H,T,Resolvente,Compatible),
+	%Si el resolvente no es la clausula vacia seguimos
+	((Resolvente\==cl([],[]))->
+		%Si la clausula no es compatible no añadimos el resolvente
+		((Compatible=no)->
+		(sld(T,Clausulas));
+		%La clausula es compatible
+		(member(Resolvente,Clausulas)->
+			%Si es miembro de las clausulas originales no añadimos el resolvente
+			sld(T,Clausulas)
+		%Si no es miembro de las clausulas originales añadimos el resolvente
+		;sld([Resolvente|T],Clausulas) ))
+	%Si el resolvente es la clausula vacia hemos acabado
+	;nl,write(Resolvente),true).
 
-%Yo diria que es la mejor
-sld(Clauses):-sld(Clauses,Clauses). %Hay que tener cuidado al generar un resolvente de que no esté entre las clasulas originales
-sld([H|T],Clausulas):-resolver(H,T,Resolvente,Compatible),
-((Resolvente\==cl([],[]))->((Compatible=no)->(sld(T,Clausulas));( member(Resolvente,Clausulas)->sld(T,Clausulas); sld([Resolvente|T],Clausulas) )); nl,write(Resolvente),true).
-
+%Toma una clausula y una lista de clausulas y devuelve el resolvente y si es compatible
+%Compatible-> ha podido efectuar un paso de resolucion con alguna clausula de la lista
 resolver(Clausula,[],Clausula,no):-!.
-resolver(Clausula,[H|_],Resolvente,ClausulaCompatible):-resolverClausulas(Clausula,H,Resolvente,ClausulaCompatible),ClausulaCompatible=si,nl,
-write(Clausula),write(" + "),write(H),write(" = "),write(Resolvente),!.
 
+%Si la clausula es compatible la imprimimos y paramos
+resolver(Clausula,[H|_],Resolvente,ClausulaCompatible):-
+	resolverClausulas(Clausula,H,Resolvente,ClausulaCompatible),
+	ClausulaCompatible=si,
+	nl, write(Clausula), write(" + "), write(H), write(" = "), write(Resolvente),!.
 
-resolver(Clausula,[H|T],Resolvente,ClausulaCompatible):-resolverClausulas(Clausula,H,_,CC),
- CC=no,
- resolver(Clausula,T,Resolvente,ClausulaCompatible),!.
+%Si no es compatible seguimos
+resolver(Clausula,[H|T],Resolvente,ClausulaCompatible):-
+	resolverClausulas(Clausula,H,_,CC),
+ 	CC=no,
+ 	resolver(Clausula,T,Resolvente,ClausulaCompatible),!.
 
-/*Resuelve dos clausulas y nos dice si son compatibles, es decir, que se puede eliminar algo de alguna*/
+%Resuelve dos clausulas y nos dice si son compatibles, es decir, que se puede eliminar algo de alguna
 resolverClausulas(cl(L1,L2),cl(L3,L4),Resolvente,ClausulaCompatible):-
+	%Unimos las listas y eliminamos duplicados
 	unirListas(L1,L3,Positivas), 
 	unirListas(L2,L4,Negativas),
+	%Eliminamos de las positivas las negativas y de las negativas las positivas
 	eliminarOcurrencias(Positivas,Negativas,PositivasSinNegativas),
 	eliminarOcurrencias(Negativas,Positivas,NegativasSinPositivas),
+	%Generamos el resolvente
 	Resolvente=cl(PositivasSinNegativas,NegativasSinPositivas),
+	%Si las longitudes de ambas listas son las mismas antes y despues
+	%de eliminar las ocurrencias es que no se ha podido quitar nada
 	length(Positivas, L_Positivas),
 	length(PositivasSinNegativas, L_PositivasSinNegativas),
 	length(Negativas, L_Negativas),
 	length(NegativasSinPositivas, L_NegativasSinPositivas),
-	((L_Positivas=:=L_PositivasSinNegativas,L_Negativas=:=L_NegativasSinPositivas)->ClausulaCompatible=no;ClausulaCompatible=si).
+	((L_Positivas=:=L_PositivasSinNegativas,L_Negativas=:=L_NegativasSinPositivas)->
+		ClausulaCompatible=no;
+		ClausulaCompatible=si).
 	
 	
-	
-	
-
+%Quita de la lista A los elementos que aparezcan en la lista B
 eliminarOcurrencias([],_,[]).
 eliminarOcurrencias([H|T],L,Salida):-
-	(member(H,L)->eliminarOcurrencias(T,L,Salida);
-	(eliminarOcurrencias(T,L,L2),Salida=[H|L2])).
+	(member(H,L)->
+		eliminarOcurrencias(T,L,Salida);
+		(eliminarOcurrencias(T,L,L2),
+	Salida=[H|L2])).
 
-/*Une dos listas y elimina los duplicados*/
-unirListas(L1,L2,L3):-append(L1,L2,L12),eliminarDuplicados(L12,L3).
+%Une dos listas y elimina los duplicados
+unirListas(L1,L2,L3):- 
+	append(L1,L2,L12),
+	eliminarDuplicados(L12,L3).
+
+%Elimina los duplicados de una lista
 eliminarDuplicados([], []).
-eliminarDuplicados([H | T], ListaSinDups) :- member(H, T), eliminarDuplicados(T, ListaSinDups),!.
-eliminarDuplicados([H | T], [H | ListaSinDups]) :- not(member(H, T)), eliminarDuplicados(T, ListaSinDups),!.
+eliminarDuplicados([H | T], ListaSinDups) :- 
+	member(H, T), 
+	eliminarDuplicados(T, ListaSinDups),!.
+eliminarDuplicados([H | T], [H | ListaSinDups]) :- 
+	not(member(H, T)), 
+	eliminarDuplicados(T, ListaSinDups),!.
